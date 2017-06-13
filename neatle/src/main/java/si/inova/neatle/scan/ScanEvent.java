9453e45b3229e7720737abdac37e2062674bdd4d
tomazs <tomaz.strazisnik@inova.si>
2017-06-13 13:37:02 +0200

    WiP: Added the ability to parse advertisement packets. Added copyrights.

--- neatle/src/main/java/si/inova/neatle/scan/ScanEvent.java
+++ neatle/src/main/java/si/inova/neatle/scan/ScanEvent.java
@@ -1,23 +1,47 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Inova IT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package si.inova.neatle.scan;
 
 import android.bluetooth.BluetoothDevice;
 import android.provider.Settings;
 
-/**
- * Created by tomazs on 17. 05. 2017.
- */
-
 public class ScanEvent {
 
     private final BluetoothDevice device;
     private final int rssi;
-    private final byte[] scanRecord;
+    private final ScanRecord scanRecord;
     private final long when;
 
     public ScanEvent (BluetoothDevice device, int rssi, byte[] scanRecord) {
         this.device = device;
         this.rssi = rssi;
-        this.scanRecord = scanRecord;
+        if (scanRecord != null && scanRecord.length > 0) {
+            this.scanRecord = ScanRecord.createFromBytes(scanRecord);
+        } else {
+            this.scanRecord = null;
+        }
+
         this.when = System.currentTimeMillis();
     }
 
@@ -29,6 +53,10 @@ public class ScanEvent {
         return rssi;
     }
 
+    public ScanRecord getScanRecord() {
+        return scanRecord;
+    }
+
     public long getWhen() {
         return when;
     }
@@ -37,4 +65,6 @@ public class ScanEvent {
     public String toString() {
         return getDevice() + ", rssi: " + getRssi() + ", advertisment " + scanRecord;
     }
+
+
 }
